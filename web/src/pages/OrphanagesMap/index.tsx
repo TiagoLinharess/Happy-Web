4a4a91ccebfaa183ab2e226fc2b8d8e1d3b564e8
TiagoLinharess <tiagolinharessouza@gmail.com>
2020-10-15 22:48:27 -0300

    Segundo commit

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus, FiMoon } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiMoon, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import MapMarkerImg from '../../images/map-marker.svg';
 
+import mapIcon from '../../utils/mapIcon';
+
 import '../../styles/pages/orphanages-map.css';
+import api from '../../services/api';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 const OrphanagesMap: React.FC = () => {
   const [theme, setTheme] = useState('streets-v11');
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    })
+  }, []);
 
   useEffect(() => {
     const timeHour = new Date().getHours();
@@ -59,13 +76,29 @@ const OrphanagesMap: React.FC = () => {
           >
             <TileLayer 
             url={`https://api.mapbox.com/styles/v1/mapbox/${theme}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+            
+            {orphanages.map(orphanage => {
+              return (
+                <Marker key={orphanage.id}
+                  icon={mapIcon}
+                  position={[orphanage.latitude,orphanage.longitude]}
+                >
+                  <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                    {orphanage.name}
+                    <Link to={`/orphanages/${orphanage.id}`}>
+                      <FiArrowRight size={20} color="#FFF" />
+                    </Link>
+                  </Popup>
+                </Marker>
+              )
+            })}
           </Map>
 
           <button className="create-orphanage change-theme" onClick={changeTheme}>
             <FiMoon size={32} color="#fff"/>
           </button>
 
-          <Link to="/" className="create-orphanage">
+          <Link to="/orphanages/create" className="create-orphanage">
             <FiPlus size={32} color="#fff"/>
           </Link>
       </div>
